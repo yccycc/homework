f4034836de3aae03b25fd46761b4e7b7240f03af
Administrator <772878320@qq.com>
2016-03-27 20:06:08 +0800

    fix bar chart bg color

---
+++ workteminal/src/com/pemt/workteminal/component/BarFrag.java
@@ -0,0 +1,95 @@
+package com.pemt.workteminal.component;
+
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import org.achartengine.ChartFactory;
+import org.achartengine.model.CategorySeries;
+import org.achartengine.model.XYMultipleSeriesDataset;
+import org.achartengine.renderer.XYMultipleSeriesRenderer;
+import org.achartengine.renderer.XYSeriesRenderer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BarFrag extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        String[] titles = new String[] { "待处理", "待上传" };
+        List<double[]> values = new ArrayList<double[]>();
+        values.add(new double[] { 185 });
+        values.add(new double[] { 10 });
+        int[] colors = new int[] { Color.BLUE, Color.CYAN };
+        XYMultipleSeriesRenderer renderer = buildBarRenderer(colors);
+        setChartSettings(renderer, "工单结果", "类型", "数量", 0,
+                2, 0, 200, Color.GRAY, Color.LTGRAY);
+        ((XYSeriesRenderer) renderer.getSeriesRendererAt(0)).setDisplayChartValues(true);
+        ((XYSeriesRenderer) renderer.getSeriesRendererAt(1)).setDisplayChartValues(true);
+        renderer.setLabelsColor(Color.BLACK);
+        renderer.setXLabels(1);
+        renderer.setYLabels(10);
+        renderer.setXLabelsAlign(Paint.Align.LEFT);
+        renderer.setYLabelsAlign(Paint.Align.LEFT);
+        renderer.removeXTextLabel(2);
+        renderer.setXLabelsColor(Color.BLACK);
+        renderer.setYLabelsColor(0,Color.BLACK);
+        renderer.setPanEnabled(true, false);
+        renderer.setZoomRate(5.1f);
+        renderer.setBarSpacing(10.0f);
+        renderer.setBarWidth(50);
+        View view = ChartFactory.getBarChartView(BarFrag.this.getActivity(),
+                buildBarDataset(titles, values), renderer, org.achartengine.chart.BarChart.Type.DEFAULT);
+        renderer.setClickEnabled(true);
+        renderer.setApplyBackgroundColor(true);//必须设置为true，颜色值才生效
+        renderer.setBackgroundColor(Color.TRANSPARENT);//设置表格背景色
+        renderer.setMarginsColor(Color.TRANSPARENT);//设置周边背景色
+        return view;
+    }
+    protected XYMultipleSeriesRenderer buildBarRenderer(int[] colors) {
+        XYMultipleSeriesRenderer renderer = new XYMultipleSeriesRenderer();
+        renderer.setAxisTitleTextSize(16);
+        renderer.setChartTitleTextSize(20);
+        renderer.setLabelsTextSize(15);
+        renderer.setLegendTextSize(15);
+        int length = colors.length;
+        for (int i = 0; i < length; i++) {
+            XYSeriesRenderer r = new XYSeriesRenderer();
+            r.setColor(colors[i]);
+            renderer.addSeriesRenderer(r);
+        }
+        return renderer;
+    }
+    protected void setChartSettings(XYMultipleSeriesRenderer renderer, String title, String xTitle,
+                                    String yTitle, double xMin, double xMax, double yMin, double yMax, int axesColor,
+                                    int labelsColor) {
+        renderer.setChartTitle(title);
+        renderer.setXTitle(xTitle);
+        renderer.setYTitle(yTitle);
+        renderer.setXAxisMin(xMin);
+        renderer.setXAxisMax(xMax);
+        renderer.setYAxisMin(yMin);
+        renderer.setYAxisMax(yMax);
+        renderer.setAxesColor(axesColor);
+        renderer.setLabelsColor(labelsColor);
+    }
+    protected XYMultipleSeriesDataset buildBarDataset(String[] titles, List<double[]> values) {
+        XYMultipleSeriesDataset dataset = new XYMultipleSeriesDataset();
+        int length = titles.length;
+        for (int i = 0; i < length; i++) {
+            CategorySeries series = new CategorySeries(titles[i]);
+            double[] v = values.get(i);
+            int seriesLength = v.length;
+            for (int k = 0; k < seriesLength; k++) {
+                series.add(v[k]);
+            }
+            dataset.addSeries(series.toXYSeries());
+        }
+        return dataset;
+    }
+}
---
+++ workteminal/src/com/pemt/workteminal/component/PieFrag.java
@@ -0,0 +1,60 @@
+package com.pemt.workteminal.component;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import com.pemt.workteminal.R;
+import org.achartengine.ChartFactory;
+import org.achartengine.model.CategorySeries;
+import org.achartengine.renderer.DefaultRenderer;
+import org.achartengine.renderer.SimpleSeriesRenderer;
+
+public class PieFrag extends Fragment {
+    private LinearLayout mChartLl;
+    private int[] mGdNums;
+    private View mParentView;
+    private final static int TO_BE_DEALED_COLOR = 0xFF4D50C0;
+    private final static int TO_BE_UPLOADED_COLOR = 0xFFBD814F;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        mParentView = inflater.inflate(R.layout.mainpage_pie,null);
+        mChartLl = (LinearLayout) mParentView.findViewById(R.id.chart_space_ll);
+        int[] colors = new int[]{TO_BE_DEALED_COLOR, TO_BE_UPLOADED_COLOR};
+        mGdNums = new int[]{90, 10};
+        DefaultRenderer renderer = buildCategoryRenderer(colors);
+        SimpleSeriesRenderer r = renderer.getSeriesRendererAt(1);
+        r.setHighlighted(true);
+        View chartView = ChartFactory.getPieChartView(PieFrag.this.getActivity(), buildCategoryDataset("工单",mGdNums), renderer);
+        mChartLl.addView(chartView);
+        return mParentView;
+    }
+    protected DefaultRenderer buildCategoryRenderer(int[] colors) {
+        DefaultRenderer renderer = new DefaultRenderer();
+        renderer.setLabelsTextSize(18);
+        renderer.setLabelsColor(Color.BLACK);
+        renderer.setLegendTextSize(15);
+        renderer.setPanEnabled(false);
+        renderer.setShowLegend(false);
+        for (int color : colors) {
+            SimpleSeriesRenderer r = new SimpleSeriesRenderer();
+            r.setColor(color);
+            renderer.addSeriesRenderer(r);
+        }
+        return renderer;
+    }
+
+    protected CategorySeries buildCategoryDataset(String title, int[] gdNums) {
+        CategorySeries series = new CategorySeries(title);
+        int k = 0;
+        for (double num : gdNums) {
+            series.add(mGdNums[k++]+"", num);
+        }
+        return series;
+    }
+}
