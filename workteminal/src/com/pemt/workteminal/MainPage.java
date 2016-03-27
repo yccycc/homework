09ad7e8a49d91af0504d137912b04ff0aec0b46f
Administrator <772878320@qq.com>
2016-03-27 19:06:25 +0800

    add viewpager

--- workteminal/src/com/pemt/workteminal/MainPage.java
+++ workteminal/src/com/pemt/workteminal/MainPage.java
@@ -1,45 +1,38 @@
 package com.pemt.workteminal;
 
-import android.app.Activity;
-import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.*;
-import org.achartengine.ChartFactory;
-import org.achartengine.model.CategorySeries;
-import org.achartengine.renderer.DefaultRenderer;
-import org.achartengine.renderer.SimpleSeriesRenderer;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.pemt.workteminal.component.BarFrag;
+import com.pemt.workteminal.component.PieFrag;
 
-public class MainPage extends Activity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainPage extends FragmentActivity {
     private GridView mMpMenuGv;
     private int mMenuItemImgs[] = {R.drawable.menu_wait_gd, R.drawable.gd_search, R.drawable.map_nav, R.drawable.job_instruction, R.drawable.system_set};
     private String mMenuLabels[];
-    private LinearLayout mChartLl;
-    private String[] mDoughNutItems;
-
-    private final static int TO_BE_DEALED_COLOR = 0xFF4D50C0;
-    private final static int TO_BE_UPLOADED_COLOR = 0xFFBD814F;
-    private int[] mGdNums;
+    private ViewPager mMpVp;
+    private List mFrgList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.mainpage_bar);
+        setContentView(R.layout.mainpage);
         String menuLabels[] = {getResources().getString(R.string.menu_wait_gd), getResources().getString(R.string.gd_search),
                 getResources().getString(R.string.map_nav), getResources().getString(R.string.job_instruction), getResources().getString(R.string.system_set)};
-        String doughNutItems[] = {getResources().getString(R.string.wait_done), getResources().getString(R.string.wait_uploaded)};
-        mDoughNutItems = doughNutItems;
         mMenuLabels = menuLabels;
         mMpMenuGv = (GridView) findViewById(R.id.mp_menu_gv);
-        mChartLl = (LinearLayout) findViewById(R.id.chart_space_ll);
-        int[] colors = new int[]{TO_BE_DEALED_COLOR, TO_BE_UPLOADED_COLOR};
-        mGdNums = new int[]{90, 10};
-        DefaultRenderer renderer = buildCategoryRenderer(colors);
-        SimpleSeriesRenderer r = renderer.getSeriesRendererAt(1);
-        r.setHighlighted(true);
-        View chartView = ChartFactory.getPieChartView(this, buildCategoryDataset("工单",mGdNums), renderer);
-        mChartLl.addView(chartView);
         mMpMenuGv.setAdapter(new BaseAdapter() {
             @Override
             public int getCount() {
@@ -66,31 +59,27 @@ public class MainPage extends Activity {
                 return convertView;
             }
         });
+        mMpVp = (ViewPager) findViewById(R.id.mp_vp);
+        mFrgList = new ArrayList<Fragment>();
+        mFrgList.add(new PieFrag());
+        mFrgList.add(new BarFrag());
+        mMpVp.setAdapter(new ChartPagerAdapter(MainPage.this.getSupportFragmentManager(),mFrgList));
 
     }
+    class ChartPagerAdapter extends FragmentPagerAdapter{
 
-    protected DefaultRenderer buildCategoryRenderer(int[] colors) {
-        DefaultRenderer renderer = new DefaultRenderer();
-        renderer.setLabelsTextSize(18);
-        renderer.setLabelsColor(Color.BLACK);
-        renderer.setLegendTextSize(15);
-        renderer.setPanEnabled(false);
-        renderer.setShowLegend(false);
-        for (int color : colors) {
-            SimpleSeriesRenderer r = new SimpleSeriesRenderer();
-            r.setColor(color);
-            renderer.addSeriesRenderer(r);
+        public ChartPagerAdapter(FragmentManager fm,List<Fragment> fragList) {
+            super(fm);
         }
-        return renderer;
-    }
 
-    protected CategorySeries buildCategoryDataset(String title, int[] gdNums) {
-        CategorySeries series = new CategorySeries(title);
-        int k = 0;
-        for (double num : gdNums) {
-            series.add(mGdNums[k++]+"", num);
+        @Override
+        public Fragment getItem(int i) {
+            return (Fragment) mFrgList.get(i);
         }
-        return series;
-    }
 
+        @Override
+        public int getCount() {
+            return mFrgList.size();
+        }
+    }
 }
